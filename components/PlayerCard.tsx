3fd2c0172f9ce49ad94b6070d03a207dd9844ff7
Oleg Bondarenko <22oleg07@gmail.com>
2025-08-16 10:19:46 -0500

    refactor: made the two halves of the card of equivalent height

--- components/PlayerCard.tsx
+++ components/PlayerCard.tsx
@@ -24,20 +24,19 @@ export function PlayerCard({ player }: { player: Player | null }) {
     const points = player.operatorSalary ? Math.max(1, Math.round(player.operatorSalary / 200)) : 23
 
     return (
-        <div className="flex flex-col items-center pt-12 w-full h-full bg-[#1D1D1D] rounded-lg overflow-hidden">
-            <div className="w-full h-[256px] relative">
+        <div className="flex flex-col items-center  w-full h-full bg-[#1D1D1D] rounded-lg overflow-hidden">
+            <div className="w-full h-[301px] relative">
                 <Image
                     src="/brady.png"
                     alt={title}
                     fill
-                    className="object-cover"
                     priority
                 />
             </div>
             <div className="w-full flex-1 bg-[#2F2F2F] rounded-t-lg flex flex-col items-center justify-start">
                 <div className="w-full h-[72px] text-[32px] leading-[72px] text-white/80 text-center">{title}</div>
                 <div className="w-full h-[172px] text-[128px] leading-[172px] text-white/80 text-center">{points}</div>
-                <div className="w-full h-4 text-[16px] leading-4 text-white/80 text-center">Points</div>
+                <div className="w-full h-8 text-[16px] leading-4 text-white/80 text-center">Points</div>
             </div>
         </div>
     )
